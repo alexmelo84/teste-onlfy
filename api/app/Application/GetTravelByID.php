3e751bd0fd4f03e141f3688d061764fb3e90b284
Alex Melo <alexmelo84@hotmail.com>
2025-01-31 11:20:31 -0300

    Recuperação de uma viagem

---
+++ api/app/Application/GetTravelByID.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Application;
+
+use App\Abstract\AbstractTravel;
+use App\Models\Travel;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * Return a trael by ID
+ */
+class GetTravelByID extends AbstractTravel
+{
+    /**
+     * @param int
+     */
+    protected int $travelID;
+
+    /**
+     * @param int $travelID
+     */
+    public function __construct(int $travelID)
+    {
+        $this->travelID = $travelID;
+    }
+
+    /**
+     * @return Travel
+     */
+    public function create(): Travel
+    {
+        try {
+            $travel = $this->getTravel();
+            if (empty($travel)) {
+                throw new Exception('A viagem não foi encontrada', 404);
+            }
+        } catch (Exception $e) {
+            throw new HttpException($e->getCode(), $e->getMessage());
+        }
+
+        return $travel;
+    }
+}
